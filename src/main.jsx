073db29f4c0ae627968bd57f004943c83ab06a53
i-amshifa-06 <shifaulkareem06@gmail.com>
2026-07-08 07:25:55 +0530

    Added Google Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,11 @@ import App from "./App.jsx";
 import "./index.css";
 import "./styles/global.css";
 
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-DXYS9B57TB");
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
